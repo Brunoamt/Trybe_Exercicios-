f30003dda981e4da4f916c5dd2381e0154d79773
Brunoamt <brunoamt2_1502@hotmil.com>
2022-01-05 12:57:24 -0300

    alguns exercicios feitos

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,12 @@
+function seletorDeEstado() {
+  let states = document.getElementById('estados-brasil');
+  let stateOptions =  ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  for (let i = 0; i < stateOptions.length; i += 1) {
+    let option = document.createElement('option');
+    option.innerText = stateOptions[i];
+    option.value = stateOptions[i];
+    states.appendChild(option);
+  }
+}
+seletorDeEstado();
